6cc68e0ead8803475cdc110e0e2a817bbf76c959
unknown <lailyakter138@gmail.com>
2024-11-14 10:18:56 +0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  description: "This is Home page",
   // other metadata
 };
 
